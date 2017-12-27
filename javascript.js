6d7077bf51ee43decb89694c666ebf4bb3d3a7ac
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2017-12-27 13:25:44 +0100

    ok

--- javascript.js
+++ javascript.js
@@ -1,17 +1,20 @@
 window.addEventListener("load", function(){
 
 
-  var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight,
-                     document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+
 
   let footer = document.getElementById("footer");
 
 
 
-                    let grans = limit - window.innerHeight
 
-                    console.log(grans)
   window.addEventListener("scroll", function(e){
+
+    var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight,
+                       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
+
+                       let grans = limit - window.innerHeight
+
 
     var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
 
