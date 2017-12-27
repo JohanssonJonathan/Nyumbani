41c3638deea6b0bf32f0742350151c8b5ce0f59c
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2017-12-27 13:15:54 +0100

    ofd

--- javascript.js
+++ javascript.js
@@ -4,14 +4,25 @@ window.addEventListener("load", function(){
   var limit = Math.max( document.body.scrollHeight, document.body.offsetHeight,
                      document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );
 
+  let footer = document.getElementById("footer");
+
+
+
                     let grans = limit - window.innerHeight
+
+                    console.log(grans)
   window.addEventListener("scroll", function(e){
 
     var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
 
     console.log(window.innerHeight)
     console.log(scrollPos)
+    if(scrollPos < grans){
+      footer.style.height = "0px"
+    }
     if(scrollPos === grans){
+      alert("hej")
+      footer.style.height = "100px"
     }
   })
 
