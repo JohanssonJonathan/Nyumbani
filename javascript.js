1002dd8ee671f9e269cf5787f6bc765f751e40b8
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2018-01-04 17:40:50 +0100

    ok

--- javascript.js
+++ javascript.js
@@ -7,7 +7,56 @@ window.addEventListener("load", function(){
   let instagram = document.getElementById("instagram")
   let facebook = document.getElementById("facebook")
 
+  let about = document.getElementById("about");
+  let contact = document.getElementById("contact");
+  let photos = document.getElementById("photos");
 
+  about.className = "current"
+  let content1 = document.getElementById("content1")
+  let content2 = document.getElementById("content2")
+  let content3 = document.getElementById("content3")
+
+  content2.className ="contentNone";
+  content3.className ="contentNone"
+
+  window.addEventListener("click",function(e){
+      e.preventDefault()
+    if(e.target === about){
+
+      about.className ="current";
+      contact.className ="";
+      photos.className ="";
+
+        content1.className = "contentBlock";
+        content2.className ="contentNone";
+        content3.className = "contentNone"
+
+
+    }
+    if(e.target === photos){
+
+      about.className ="";
+      contact.className ="";
+      photos.className ="current";
+
+      content2.className = "contentBlock";
+      content1.className ="contentNone";
+      content3.className = "contentNone"
+    }
+
+    if(e.target === contact){
+
+      about.className ="";
+      contact.className ="current";
+      photos.className ="";
+
+      content3.className = "contentBlock";
+      content2.className ="contentNone";
+      content1.className = "contentNone"
+
+    }
+
+  })
 
 
   window.addEventListener("scroll", function(e){
@@ -27,7 +76,7 @@ window.addEventListener("load", function(){
       footer.style.height = "70px"
       instagram.style.height ="50px"
       facebook.style.height = "50px"
-  
+
 
     }else if( scrollMax < grans){
       footer.style.height = "0px"
