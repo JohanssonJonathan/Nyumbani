d8d5d3981753f22cf1840b090aba1fcc65e58baf
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2018-01-10 10:44:28 +0100

    ok

--- javascript.js
+++ javascript.js
@@ -1,43 +1,47 @@
 window.addEventListener("load", function(){
 
-  let body = document.getElementsByTagName("body")[0]
-  let image = document.getElementsByClassName("productBild");
-
-  let storBild = document.getElementById("storBild")
-  let diven = document.getElementsByClassName("storaBilder")[0];
-
-  let widthOnWindow = window.innerWidth;
-
-
-  for(let i=0; i< image.length;i++){
-
-    let bild = document.createElement("img");
-
-    let x = 0;
-
-      image[i].addEventListener("click", function(e){
-
-          let splittad = e.srcElement.src.split("/")
-          bild.setAttribute("src","Bilder/Products/" + splittad[12]);
-          storBild.appendChild(bild)
-          diven.style.display = "flex";
-
-      })
-
-
-      diven.addEventListener("click",function(){
-
-            diven.style.display = "none"
-            storBild.innerHTML = "";
-
-      })
 
 
 
+
+/* Hämtar div för att kunna lägga in stor bild */
+  let storBild = {
+
+    image: document.getElementsByClassName("productBild"),
+    bild : document.getElementById("innehall"),
+    content : document.getElementsByClassName("storaBilder")[0]
 
   }
 
 
+
+  /***********Går igenom bilderna på Products *****
+  När man har klickat på en bild så ska de bli större bild.
+  ****/
+
+  for(let i=0; i< storBild.image.length;i++){
+
+    let img = document.createElement("img");
+
+      storBild.image[i].addEventListener("click", function(e){
+
+          let splittad = e.srcElement.src.split("/")
+          img.setAttribute("src","Bilder/Products/" + splittad[12]);
+          storBild.bild.appendChild(img)
+          storBild.content.style.display = "flex";
+
+      })
+
+      storBild.content.addEventListener("click",function(){
+
+            storBild.content.style.display = "none"
+            storBild.bild.innerHTML = "";
+
+      })
+  }
+
+
+  /* Byte Av BILD på headern var femte sekund. */
 
   let headerBilder = ["people.png","gnu.jpg","lejon.png","monkey.png","elefant2.png"]
 
@@ -45,11 +49,10 @@ window.addEventListener("load", function(){
 
   let logga = document.getElementById("logga")
 
-  let i =0;
+
+  let i =0; /* Håller koll på vilken bild vi är på */
 
 let interval =  setInterval(function(){
-
-
 
       header.style.background = (`url(Bilder/Nyumbaniskate/${headerBilder[i]})`);
 
@@ -57,49 +60,42 @@ let interval =  setInterval(function(){
       header.style.backgroundPosition = "center 0";
       header.style.backgroundSize = "100%";
 
-      if(headerBilder[i] === "people.png"){
 
-        logga.style.opacity = "0"
-      }else{
-        logga.style.opacity = "1"
-      }
-
-      if(headerBilder[i] === "gnu.jpg"){
-
-          header.style.backgroundPosition = "center -100px"
-
-      }else{
-          header.style.backgroundPosition = "center 0";
-      }
-
-      if(headerBilder.length - 1 === i ){
-        i=0
-      }else{
-        i++
-      }
+      /* Tar bort loggan när vi är på people bilden. För att kunna se ansiktena */
+      (headerBilder[i] === "people.png") ? logga.style.opacity = "0" : logga.style.opacity = "1";
 
 
+      /* Fixar till Gnu bilden */
+(headerBilder[i] === "gnu.jpg") ? header.style.backgroundPosition = "center -100px" : header.style.backgroundPosition = "center 0";
+
+
+      /* För att bilderna ska gå runt och börja om igen. Ökar i eller sätter den till 0 för att börja om*/
+      (headerBilder.length - 1 === i ) ? i=0 : i++
 
   },5000)
 
 
 
 
-  let footer = document.getElementById("footer");
-  let instagram = document.getElementById("instagram")
-  let facebook = document.getElementById("facebook")
+
 
   let about = document.getElementById("about");
   let contact = document.getElementById("contact");
   let photos = document.getElementById("photos");
 
-  about.className = "current"
+
   let content1 = document.getElementById("content1")
   let content2 = document.getElementById("content2")
   let content3 = document.getElementById("content3")
 
+  about.className = "current" /* Vi börjar på about */
+
+
   content2.className ="contentNone";
   content3.className ="contentNone"
+
+
+/* För att man ska kunna klicka på About, Products och contact. */
 
   window.addEventListener("click",function(e){
 
@@ -159,6 +155,10 @@ let interval =  setInterval(function(){
 
   })
 
+
+  let footer = document.getElementById("footer");
+  let instagram = document.getElementById("instagram")
+  let facebook = document.getElementById("facebook")
 
   window.addEventListener("scroll", function(e){
 
