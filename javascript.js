724db5981fd2dd7298664531e72295311af94db0
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2018-01-10 13:24:33 +0100

    ok

--- javascript.js
+++ javascript.js
@@ -28,11 +28,12 @@ window.addEventListener("load", function(){
 
           let splittad = e.srcElement.src.split("/")
           img.setAttribute("src","Bilder/Products/" + splittad[12]);
-          console.log(e.srcElement.src)
+          console.log("Detta är splittad 12 " + splittad[12])
+          console.log("Detta är e.srcElement.src " + e.srcElement.src)
           storBild.bild.appendChild(img)
           storBild.content.style.display = "flex";
 
-          console.log(img)
+          console.log("Image bilden " + img)
 
 
       })
